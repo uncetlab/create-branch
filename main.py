648c37ccd91a23ab8586f4fd17547a60720abba0
DanielSanchez97 <arturo97@live.unc.edu>
2020-06-04 11:23:44 -0400

    change comparison to None

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ tag = os.getenv("INPUT_TAG")
 print(commithash)
 print(tag)
 
-if commithash == "" and tag == "":
+if commithash is None and tag is None:
     print("Must provide either a commit hash or tag")
     sys.exit(1)
 
-if (not commithash == "") and (not tag == ""):
+if (not commithash is None) and (not tag is None):
     print("Can not provide both a commit hash and tag")
     sys.exit(1)
 
-os.environ['COMMIT'] =  commithash if commithash != "" else tag
+os.environ['COMMIT'] =  commithash if commithash is None else tag
 os.environ['BRANCH'] = os.getenv("INPUT_BRANCH")
 os.system("./createtmpbranch.sh")
 

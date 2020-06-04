dba561f6ef501e3df1d60b67f679c9bae15714f7
DanielSanchez97 <arturo97@live.unc.edu>
2020-06-04 11:27:18 -0400

    fix logical error

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if (not commithash is None) and (not tag is None):
     print("Can not provide both a commit hash and tag")
     sys.exit(1)
 
-os.environ['COMMIT'] =  commithash if commithash is None else tag
+os.environ['COMMIT'] =  commithash if (not commithash is None) else tag
 os.environ['BRANCH'] = os.getenv("INPUT_BRANCH")
 os.system("./createtmpbranch.sh")
 
